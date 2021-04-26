63bd61c7442f627e484c51e0d284cfe406bc7551
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-04-26 16:49:40 +0300

    problem have been solved

--- app/src/main/java/com/kumsal/liketwitterprofile_motionlayout/MainActivity.kt
+++ app/src/main/java/com/kumsal/liketwitterprofile_motionlayout/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kumsal.liketwitterprofile_motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    var viewPager2: ViewPager?=null
+    var adapter:SectionPagerAdapter?=null
+    var tabLayout:TabLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPager2=findViewById(R.id.pagerView)
+        tabLayout=findViewById(R.id.tabLayout)
+        adapter= SectionPagerAdapter(supportFragmentManager)
+        tabLayout?.setTabsFromPagerAdapter(adapter)
+        tabLayout?.setupWithViewPager(viewPager2)
     }
 }
--- app/src/main/java/com/kumsal/liketwitterprofile_motionlayout/SectionPagerAdapter.kt
+++ app/src/main/java/com/kumsal/liketwitterprofile_motionlayout/SectionPagerAdapter.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.PagerAdapter
 
-class SectionPagerAdapter(fm:FragmentManager): FragmentPagerAdapter(fm) {
+class SectionPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
         return 3
     }
 
     override fun getItem(position: Int): Fragment {
-        return when
+        return when(position){
+            1->Fragment_first()
+            2->Fragment_seccond()
+            3->Fragment_third()
+            else -> Fragment_first()
+        }
     }
 }
